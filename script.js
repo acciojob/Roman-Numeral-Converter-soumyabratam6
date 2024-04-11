33e2538acf4e3e9d4caa0a87db9305ba40a9f99f
soumyabratam6 <144446453+soumyabratam6@users.noreply.github.com>
2024-04-11 09:44:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
-funconst obj = {
-  0: ['M', 1000],
-  1: ['CM', 900],
-  2: ['D', 500],
-  3: ['CD', 400],
-  4: ['C', 100],
-  5: ['XC', 90],
-  6: ['L', 50],
-  7: ['XL', 40],
-  8: ['X', 10],
-  9: ['IX', 9],
-  10: ['V', 5],
-  11: ['IV', 4],
-  12: ['I', 1]
-};
+const obj = {
+    0: ['M', 1000],
+    1: ['CM', 900],
+    2: ['D', 500],
+    3: ['CD', 400],
+    4: ['C', 100],
+    5: ['XC', 90],
+    6: ['L', 50],
+    7: ['XL', 40],
+    8: ['X', 10],
+    9: ['IX', 9],
+    10: ['V', 5],
+    11: ['IV', 4],
+    12: ['I', 1]
+  };
 
-  //your code here
-   let result = '';
+  let result = '';
 
-  // Iterate through symbols
-  for (let i = 0; i <13; i++) {
+  for (let i = 0; i < 13; i++) {
     let symbol = obj[i][0];
     let value = obj[i][1];
-    // While input number is greater than or equal to current value
+
     while (num >= value) {
-      // Subtract value from num and add corresponding symbol to result
       result += symbol;
       num -= value;
     }
