3f5a7eb5b6e21dcd0bf3ca5d43ddd08245a57eaf
soumyabratam6 <144446453+soumyabratam6@users.noreply.github.com>
2024-04-11 09:29:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,14 +6,20 @@ function convertToRoman(num) {
       3:['L', 50], 
       4:['X', 10], 
       5:['V', 5], 
-      6:['I', 1]
+      6:['I', 1],
+	  7:['IV', 4],
+	  8:['IX', 9],
+	  9:['XL', 40],
+	  10['XC', 90],
+	  11:['CD', 400],
+	  12:['CM', 900]
     };
 
   //your code here
    let result = '';
 
   // Iterate through symbols
-  for (let i = 0; i <7; i++) {
+  for (let i = 0; i <13; i++) {
     let symbol = obj[i][0];
     let value = obj[i][1];
     // While input number is greater than or equal to current value
